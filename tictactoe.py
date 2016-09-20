27afa03ee525459b4373d36f44f5be29413c092d
rallen0150 <rallen0150@gmail.com>
2016-09-20 17:05:20 -0400

    second commit

--- tictactoe.py
+++ tictactoe.py
@@ -4,24 +4,30 @@ theBoard = {'00': ' ', '01': ' ', '02': ' ',
             '10': ' ', '11': ' ', '12': ' ',
             '20': ' ', '21': ' ', '22': ' '}
 
+
+# The Board code is inspired by the book to use it as a dictionary
 def printBoard(board):
-    print("  0 1 2")
-    print("0 " + board['00'] + '|' + board['01'] + '|' + board['02'])
-    print('  -+-+-')
-    print("1 " + board['10'] + '|' + board['11'] + '|' + board['12'])
-    print('  -+-+-')
-    print("2 " + board['20'] + '|' + board['21'] + '|' + board['22'])
+    print("   0   1   2")
+    print("0 " + board['00'] + '  | ' + board['01'] + ' |' + board['02'])
+    print('  ---+---+---')
+    print("1 " + board['10'] + '  | ' + board['11'] + ' |' + board['12'])
+    print('  ---+---+---')
+    print("2 " + board['20'] + '  | ' + board['21'] + ' |' + board['22'])
 
-player = 'X'
-for i in range(9):
-    printBoard(theBoard)
-    player_move = input('Player {}'.format(player) + '. Place your spot on a coordinate: ')
-    while theBoard[player_move] != ' ':
-        player_move = input("Invalid Move! Try another coordinate: ")
 
-    theBoard[player_move] = player
+def changePlayer(board, player):
     if player == 'X':
         player = 'O'
     else:
         player = 'X'
+
+player = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    player_move = input('Player {}'.format(player) + ', Place your spot on a coordinate: ')
+    while theBoard[player_move] != ' ':
+        player_move = input("Invalid Move! Try another coordinate: ")
+    theBoard[player_move] = player
+    player = changePlayer(theBoard, player)
+    player
 printBoard(theBoard)
