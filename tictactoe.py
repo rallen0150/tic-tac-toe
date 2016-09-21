a89a13097928fd5a17b20ff3eb941e11d51c9a05
rallen0150 <rallen0150@gmail.com>
2016-09-21 18:57:04 -0400

    finished win conditions

--- tictactoe.py
+++ tictactoe.py
@@ -24,19 +24,69 @@ def playerMove(board, player):
     myBoard[player_move_row][player_move_column] = new_player
 
 
+def winHorizontal(board, player):
+    for row in myBoard:
+        if row == ["X", "X", "X"] or row == ["O", "O", "O"]:
+            print("Player {} Wins by Horizontal Line!!".format(player))
+            end_game = 0
+            return end_game
+    return 1
+
+def winVertical(board, player):
+    if myBoard[0][0] == new_player and myBoard[1][0] == new_player and myBoard[2][0] == new_player:
+        print("Player {} Wins by Vertical Line!!".format(new_player))
+        end_game = 0
+        return end_game
+    elif myBoard[0][1] == new_player and myBoard[1][1] == new_player and myBoard[2][1] == new_player:
+        print("Player {} Wins by Vertical Line!!".format(new_player))
+        end_game = 0
+        return end_game
+    elif myBoard[0][2] == new_player and myBoard[1][2] == new_player and myBoard[2][2] == new_player:
+        print("Player {} Wins by Vertical Line!".format(new_player))
+        end_game = 0
+        return end_game
+    else:
+        return 1
+
+
+def winDiagonal(board, player):
+    if myBoard[0][0] == new_player and myBoard[1][1] == new_player and myBoard[2][2] == new_player:
+        print("Player {} Wins by Diagonal!".format(new_player))
+        end_game = 0
+        return end_game
+    elif myBoard[0][2] == new_player and myBoard[1][1] == new_player and myBoard[2][0] == new_player:
+        print("Player {} Wins by Diagonal!".format(new_player))
+        end_game = 0
+        return end_game
+    else:
+        return 1
+
 # The Board code is inspired by the book but wanted to try it as a matrix and not a dictionary
 myBoard = [[" ", " ", " "],
            [" ", " ", " "],
            [" ", " ", " "]]
+# new_board = printBoard(myBoard)
 
 
+# blank_board = [[" ", " ", " "],
+#                [" ", " ", " "],
+#                [" ", " ", " "]]
+
+end_game_h = 1
+end_game_v = 1
+end_game_d = 1
 player = ""
 turns = 0
 
-while turns < 9:
+while turns < 9 and end_game_h == 1 and end_game_v == 1 and end_game_d == 1:
     new_player = changePlayer(player)
     printBoard(myBoard)
     playerMove(myBoard, new_player)
 
+    end_game_h = winHorizontal(myBoard, new_player)
+    end_game_v = winVertical(myBoard, new_player)
+    end_game_d = winDiagonal(myBoard, new_player)
+
     turns += 1
+
 printBoard(myBoard)
